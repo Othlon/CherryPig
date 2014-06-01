793c5432022fd7c548227321e85494d648844f92
Othlon <jen@sutherland-studios.com.au>
2014-06-02 02:54:02 +1000

    cherrytree log work

--- src/main/java/othlon/cherrypig/blocks/CPBlocks.java
+++ src/main/java/othlon/cherrypig/blocks/CPBlocks.java
@@ -3,7 +3,14 @@ package othlon.cherrypig.blocks;
 /**
  * Created by Jen on 30/05/14.
  */
+
+import cpw.mods.fml.common.registry.GameRegistry;
+import net.minecraft.block.Block;
+import othlon.cherrypig.blocks.CPCherryLog;
 public class CPBlocks {
 
     //used for initalising the blocks for use elsewhere
+    public final static Block cherryLog = new CPCherryLog();
+
+   // GameRegistry.registerBlock(CPCherryLog, 'cherryLog');
 }
--- src/main/java/othlon/cherrypig/blocks/CPCherryLog.java
+++ src/main/java/othlon/cherrypig/blocks/CPCherryLog.java
@@ -1,6 +1,8 @@
 package othlon.cherrypig.blocks;
 
+import cpw.mods.fml.common.registry.GameRegistry;
 import net.minecraft.block.Block;
+import net.minecraft.block.material.Material;
 
 /**
  * Created by Jen on 30/05/14.
@@ -9,13 +11,16 @@ public class CPCherryLog extends Block{
 
    public CPCherryLog()
    {
-       //step sound
 
        //material
-
-       //hardness
-
+       super(Material.wood);
+        //step sound
+       setStepSound(soundTypeWood);
        //creative tab
+       setBlockTextureName("cherrypig:cherrypig_treebark");
+       //set unlocalised name
+       setBlockName("CherryPig Log");
+
    }
 
 }
