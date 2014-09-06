108dd5bc79667fdff97104c52fd8cbb64cb58d81
Othlon <jen@sutherland-studios.com.au>
2014-09-06 17:22:37 +1000

    textures and improved model

--- src/main/java/othlon/cherrypig/render/CPPiggyModel.java
+++ src/main/java/othlon/cherrypig/render/CPPiggyModel.java
@@ -8,6 +8,7 @@ import othlon.cherrypig.entity.CPEntityPiggy;
 public class CPPiggyModel extends ModelBase {
 
     //fields
+//fields
     ModelRenderer body;
     ModelRenderer face;
     ModelRenderer back;
@@ -15,69 +16,76 @@ public class CPPiggyModel extends ModelBase {
     ModelRenderer left;
     ModelRenderer top;
     ModelRenderer snout;
+    ModelRenderer btm;
 
     public CPPiggyModel()
     {
         textureWidth = 64;
         textureHeight = 32;
 
-        body = new ModelRenderer(this, 0, 0);
+        body = new ModelRenderer(this, 28, 16);
         body.addBox(0F, 0F, 0F, 8, 8, 8);
         body.setRotationPoint(-3F, 15F, -3F);
         body.setTextureSize(64, 32);
         body.mirror = true;
         setRotation(body, 0F, 0F, 0F);
-        face = new ModelRenderer(this, 50, 0);
+        face = new ModelRenderer(this, 0, 0);
         face.addBox(0F, 0F, 0F, 6, 6, 1);
         face.setRotationPoint(-2F, 16F, -4F);
         face.setTextureSize(64, 32);
         face.mirror = true;
         setRotation(face, 0F, 0F, 0F);
-        back = new ModelRenderer(this, 0, 0);
+        back = new ModelRenderer(this, 14, 0);
         back.addBox(0F, 0F, 0F, 6, 6, 1);
         back.setRotationPoint(-2F, 16F, 5F);
         back.setTextureSize(64, 32);
         back.mirror = true;
         setRotation(back, 0F, 0F, 0F);
-        right = new ModelRenderer(this, 14, 20);
-        right.addBox(0F, 0F, 0F, 1, 6, 6);
-        right.setRotationPoint(-4F, 16F, -2F);
+        right = new ModelRenderer(this, 14, 7);
+        right.addBox(0F, 0F, 0F, 6, 6, 1);
+        right.setRotationPoint(-4F, 16F, 4F);
         right.setTextureSize(64, 32);
         right.mirror = true;
-        setRotation(right, 0F, 0F, 0F);
-        left = new ModelRenderer(this, 0, 20);
-        left.addBox(0F, 0F, 0F, 1, 6, 6);
-        left.setRotationPoint(5F, 16F, -2F);
+        setRotation(right, 0F, 1.570796F, 0F);
+        left = new ModelRenderer(this, 0, 7);
+        left.addBox(0F, 0F, 0F, 6, 6, 1);
+        left.setRotationPoint(5F, 16F, 4F);
         left.setTextureSize(64, 32);
         left.mirror = true;
-        setRotation(left, 0F, 0F, 0F);
-        top = new ModelRenderer(this, 0, 0);
-        top.addBox(0F, 0F, 0F, 6, 1, 6);
-        top.setRotationPoint(-2F, 14F, -2F);
+        setRotation(left, 0F, 1.570796F, 0F);
+        top = new ModelRenderer(this, 0, 14);
+        top.addBox(0F, 0F, 0F, 6, 6, 1);
+        top.setRotationPoint(-2F, 15F, -2F);
         top.setTextureSize(64, 32);
         top.mirror = true;
-        setRotation(top, 0F, 0F, 0F);
-        snout = new ModelRenderer(this, 50, 7);
+        setRotation(top, 1.570796F, 0F, 0F);
+        snout = new ModelRenderer(this, 28, 0);
         snout.addBox(0F, 0F, 0F, 4, 3, 1);
         snout.setRotationPoint(-1F, 19F, -5F);
         snout.setTextureSize(64, 32);
         snout.mirror = true;
         setRotation(snout, 0F, 0F, 0F);
+        btm = new ModelRenderer(this, 14, 14);
+        btm.addBox(0F, 0F, 0F, 6, 6, 1);
+        btm.setRotationPoint(-2F, 24F, -2F);
+        btm.setTextureSize(64, 32);
+        btm.mirror = true;
+        setRotation(btm, 1.570796F, 0F, 0F);
     }
 
-    @Override
-    public void render(Entity p_78088_1_, float p_78088_2_, float p_78088_3_, float p_78088_4_, float p_78088_5_, float p_78088_6_, float p_78088_7_)
-    {
-        super.render(p_78088_1_, p_78088_2_, p_78088_3_, p_78088_4_, p_78088_5_, p_78088_6_, p_78088_7_);
-        this.setRotationAngles(p_78088_2_, p_78088_3_, p_78088_4_, p_78088_5_, p_78088_6_, p_78088_7_, p_78088_1_);
 
-        body.render(p_78088_7_);
-        face.render(p_78088_7_);
-        back.render(p_78088_7_);
-        right.render(p_78088_7_);
-        left.render(p_78088_7_);
-        top.render(p_78088_7_);
-        snout.render(p_78088_7_);
+    public void render(Entity entity, float f, float f1, float f2, float f3, float f4, float f5)
+    {
+        super.render(entity, f, f1, f2, f3, f4, f5);
+        setRotationAngles(f, f1, f2, f3, f4, f5, entity);
+        body.render(f5);
+        face.render(f5);
+        back.render(f5);
+        right.render(f5);
+        left.render(f5);
+        top.render(f5);
+        snout.render(f5);
+        btm.render(f5);
     }
 
     private void setRotation(ModelRenderer model, float x, float y, float z)
