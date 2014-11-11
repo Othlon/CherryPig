c96ec9aeb177881c078f7995846b47e72b6bf5b9
Othlon Nova <othlon@gmail.com>
2014-11-11 19:33:04 +1100

    TRYING to get my TE's to render iun inventory and hand before i did more chicken stuf but noooooooo

--- src/main/java/othlon/cherrypig/proxies/ClientProxy.java
+++ src/main/java/othlon/cherrypig/proxies/ClientProxy.java
@@ -2,7 +2,11 @@ package othlon.cherrypig.proxies;
 
 import cpw.mods.fml.client.registry.ClientRegistry;
 import cpw.mods.fml.client.registry.RenderingRegistry;
+import net.minecraft.item.Item;
+import net.minecraftforge.client.MinecraftForgeClient;
+import othlon.cherrypig.blocks.CPBlocks;
 import othlon.cherrypig.blocks.CPFirtileEgg;
+import othlon.cherrypig.blocks.CPNest;
 import othlon.cherrypig.entity.CPEntityPiggy;
 import othlon.cherrypig.render.CPFirtileEggRender;
 import othlon.cherrypig.render.CPNestRender;
@@ -15,7 +19,13 @@ public class ClientProxy extends CommonProxy{
 
        public void registerRenderers(){
            RenderingRegistry.registerEntityRenderingHandler(CPEntityPiggy.class, new CPPiggyRender());
+
            ClientRegistry.bindTileEntitySpecialRenderer(CPNestTileEntity.class, new CPNestRender());
+           MinecraftForgeClient.registerItemRenderer(Item.getItemFromBlock(CPBlocks.nest), WHAT DO YOU WANT HERE!?);
+
+
            ClientRegistry.bindTileEntitySpecialRenderer(CPFirtileEggTileEntity.class, new CPFirtileEggRender());
+           MinecraftForgeClient.registerItemRenderer(Item.getItemFromBlock(CPBlocks.egg), Item.getItemFromBlock(CPBlocks.egg));
+
        }
 }
--- src/main/java/othlon/cherrypig/render/CPFirtileEggRender.java
+++ src/main/java/othlon/cherrypig/render/CPFirtileEggRender.java
@@ -1,10 +1,17 @@
 package othlon.cherrypig.render;
 
 import cpw.mods.fml.client.FMLClientHandler;
+import javafx.geometry.BoundingBox;
+import net.minecraft.block.Block;
+import net.minecraft.client.renderer.RenderBlocks;
+import net.minecraft.client.renderer.Tessellator;
 import net.minecraft.client.renderer.tileentity.TileEntitySpecialRenderer;
+import net.minecraft.item.ItemStack;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.ResourceLocation;
 
+import net.minecraftforge.client.IItemRenderer;
+import net.minecraftforge.common.util.ForgeDirection;
 import org.lwjgl.opengl.GL11;
 
 import othlon.cherrypig.blocks.CPBlocks;
@@ -27,7 +34,6 @@ public class CPFirtileEggRender extends TileEntitySpecialRenderer {
         renderModelAt((CPFirtileEggTileEntity)tileentity, d0, d1, d2, f);
     }
 
-
     public void renderModelAt(CPFirtileEggTileEntity tile, double dX, double dY, double dZ, float f){
 
         GL11.glPushMatrix(); // start
@@ -46,4 +52,37 @@ public class CPFirtileEggRender extends TileEntitySpecialRenderer {
         }
         GL11.glPopMatrix(); // end
     }
+
+
+
+    private void renderItem(float x, float y, float z){
+        GL11.glPushMatrix();
+        GL11.glTranslatef(x, y, z);
+        GL11.glEnable(GL11.GL_BLEND);
+        GL11.glBlendFunc(GL11.GL_SRC_ALPHA, GL11.GL_ONE_MINUS_SRC_ALPHA);
+        this.model.renderAll();
+        GL11.glDisable(GL11.GL_BLEND);
+        GL11.glPopMatrix();
+
+    }
+
+
+   // @Override
+    public boolean handleRenderType(ItemStack item, IItemRenderer type){
+        return true;
+    }
+
+  //  @Override
+    public boolean shouldUseRenderHelp(IItemRenderer.ItemRenderType type, ItemStack item, IItemRenderer.ItemRendererHelper helper){
+
+        return true;
+    }
+
+  ///  @Override
+    public void renderItem(IItemRenderer.ItemRenderType type, ItemStack item, Object... data){
+        renderItem(0, 0, 0);
+    }
+
+
+
 }
